66e8b80100868be6fb2969cc4a9defbce422c027
Ismael <maelcosta96@gmail.com>
2023-04-06 17:45:51 -0300

    docs: fix storybook deploy

--- storybook-static/assets/preview-b430f62b.js
+++
@@ -1,2 +0,0 @@
-const o={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},options:{storySort:{order:["Introduction"]}}}};export{o as default};
-//# sourceMappingURL=preview-b430f62b.js.map
--- storybook-static/sb-addons/storybook-9/manager-bundle.mjs
+++
@@ -1,4 +0,0 @@
-try{
-var p=__STORYBOOKADDONS__,{addons:a,types:_,mockChannel:h}=__STORYBOOKADDONS__;var b=__STORYBOOKTHEMING__,{CacheProvider:u,ClassNames:S,Global:k,ThemeProvider:x,background:R,color:y,convert:C,create:o,createCache:N,createGlobal:v,createReset:w,css:B,darken:D,ensure:G,ignoreSsrWarning:K,isPropValid:Y,jsx:j,keyframes:A,lighten:I,styled:P,themes:E,typography:H,useTheme:M,withTheme:U}=__STORYBOOKTHEMING__;var s=o({base:"light",brandTitle:"React Aurora",brandUrl:"https://www.npmjs.com/package/react-aurora-ts",brandImage:"https://live.staticflickr.com/65535/52786414760_8d69a1f9b7.jpg",brandTarget:"_self"});a.setConfig({theme:s});
-}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
-//# sourceMappingURL=manager-bundle.mjs.map
