5dd2a9c3de4ddcbbd0486bebd67803a1aa96ff78
Ismael <maelcosta96@gmail.com>
2023-04-06 17:21:20 -0300

    docs: improve storybook

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'React Aurora',
+  brandUrl: 'https://www.npmjs.com/package/react-aurora-ts',
+  brandImage: 'https://live.staticflickr.com/65535/52786414760_8d69a1f9b7.jpg',
+  brandTarget: '_self',
+});
